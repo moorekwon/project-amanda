87b25911f6c7335204eee805b160468fdf513527
moorekwon <raccoonhj33@gmail.com>
2020-07-31 01:31:39 +0900

    members admin 추가

--- app/members/admin.py
+++ app/members/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from members.models import (
+    Member, MemberInfo, MemberImage, MemberRibbon, Star, Pick, Tag, TagTypeSelection, Story
+)
+
+admin.site.register(Member)
+admin.site.register(MemberInfo)
+admin.site.register(MemberImage)
+admin.site.register(MemberRibbon)
+admin.site.register(Star)
+admin.site.register(Pick)
+admin.site.register(Tag)
+admin.site.register(TagTypeSelection)
+admin.site.register(Story)
